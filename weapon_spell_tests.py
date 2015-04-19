5d5926ff4a38bb87f0264878037b16e35eaab473
vassi95 <vassi__95@abv.bg>
2015-04-19 20:26:35 +0300

    add tests

--- weapon_spell_tests.py
+++ weapon_spell_tests.py
@@ -4,22 +4,22 @@ import unittest
 
 class weapon_test(unittest.TestCase):
     def setUp(self):
-        self.weapon = Weapon("Weapon", 20)
+        self.weapon = Weapon("Weapon", 30)
 
     def test_init(self):
         self.assertEqual(self.weapon.name, "Weapon")
-        self.assertEqual(self.weapon.damage, 20)
+        self.assertEqual(self.weapon.damage, 30)
 
 
 class spell_test(unittest.TestCase):
     def setUp(self):
-        self.spell = Spell("Spell", 100, 20, 3)
+        self.spell = Spell("Spell", 100, 30, 2)
 
     def test_init(self):
         self.assertEqual(self.spell.name, "Spell")
         self.assertEqual(self.spell.damage, 100)
-        self.assertEqual(self.spell.mana_cost, 20)
-        self.assertEqual(self.spell.cast_range, 3)
+        self.assertEqual(self.spell.mana_cost, 30)
+        self.assertEqual(self.spell.cast_range, 2)
 
 if __name__ == '__main__':
     unittest.main()
